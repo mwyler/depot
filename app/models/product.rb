bab8744971843650a6bc0919e452c8624201c305
mwyler <wyler.m@bluewin.ch>
2011-04-28 23:08:43 +0200

    * Added cart and line items

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,18 @@
 class Product < ActiveRecord::Base
   default_scope :order => 'title'
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :title, :uniqueness => true
   validates :image_url, :format => {:with => %r{\.(gif|jpg|png)$}i,:message => 'Geben Sie dir URl zu einem JPG, GIF oder PNG file ein. bitte.'}
+  private
+  def ensure_not_referenced_by_any_line_item
+    if line_items.count.zero?
+      return true
+    else
+      errors.add(:base, 'Line items present')
+      return false
+    end
+  end
 end
