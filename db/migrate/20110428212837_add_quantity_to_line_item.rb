22d0b48f583358a621b303d8623b7b32cec68d35
mwyler <wyler.m@bluewin.ch>
2011-04-29 00:01:37 +0200

    * oops.. forgot to save * fixed typo bug.. once again..

--- db/migrate/20110428212837_add_quantity_to_line_item.rb
+++ db/migrate/20110428212837_add_quantity_to_line_item.rb
@@ -1,6 +1,6 @@
 class AddQuantityToLineItem < ActiveRecord::Migration
   def self.up
-    add_column :line_items, :quantity, :integer
+    add_column :line_items, :quantity, :integer, :default => 1
   end
 
   def self.down
